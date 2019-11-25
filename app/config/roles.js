dcf1e37ca92dbeb14eeadfc60569b5c8ce2c1340
Michaël Dierick <michael.dierick@redpencil.io>
2019-11-25 13:59:45 +0100

    Allow agenda edit for Kanselarij

--- app/config/roles.js
+++ app/config/roles.js
@@ -1,7 +1,7 @@
 export default {
   "450915b2-4c64-4b03-9caa-71180400f831": {
     canAccessApplication: false,
-    canEditAgenda: true,
+    canEditAgenda: false,
     canAccessSettings: false,
   },
   "71c068e6-d2f0-43de-93ab-cd1e7156ac4b": {
@@ -11,7 +11,7 @@ export default {
   },
   "50f4c79c-902d-4ad2-bca1-0f37a69f0c13": {
     canAccessApplication: true,
-    canEditAgenda: false,
+    canEditAgenda: true,
     canAccessSettings: false,
   },
   "7e8c0c9c-05ec-49fd-9e96-fc54ebf3f9eb": {
