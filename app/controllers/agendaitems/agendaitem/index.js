c498c4d783f1aff27de39b44e428286240458d57
Michaël Dierick <michael.dierick@redpencil.io>
2019-11-25 14:51:11 +0100

    Default notification document type

--- app/controllers/agendaitems/agendaitem/index.js
+++ app/controllers/agendaitems/agendaitem/index.js
@@ -1,10 +1,18 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { NOTIFICATION_TYPE_ID } from 'frontend-overlegcomite/config/constants';
 
 export default Controller.extend({
   addingDocuments: false,
   addingNotification: false,
   currentSession: service(),
+  store: service(),
+
+  init() {
+    this._super(...arguments);
+    const notificationType = this.store.findRecord('document-type', NOTIFICATION_TYPE_ID);
+    this.set('notificationType', notificationType);
+  },
 
   actions: {
     async addDocuments(documents) {
