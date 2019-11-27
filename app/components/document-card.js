6892fe8d8e7d7fcd1cb8c179e381431148acca43
Michaël Dierick <michael.dierick@redpencil.io>
2019-11-27 14:53:32 +0100

    Fix context on component property

--- app/components/document-card.js
+++ app/components/document-card.js
@@ -39,7 +39,7 @@ export default class extends Component {
 
   get selectedVersion() {
     if (this.args.selectedVersion) {
-      return document.documentVersions.findBy('id', this.args.selectedVersion.get('id'))
+      return this.document.documentVersions.findBy('id', this.args.selectedVersion.get('id'))
     } else {
       return this.document.lastDocumentVersion;
     }
