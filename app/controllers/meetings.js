b86408d5a0e1163fde6b883d59f554b6d256a1a6
Michaël Dierick <michael.dierick@redpencil.io>
2020-02-21 10:11:37 +0100

    Add ability to filter meetings by date

--- app/controllers/meetings.js
+++ app/controllers/meetings.js
@@ -1,18 +1,54 @@
 import Controller from "@ember/controller";
 import DefaultQueryParamsMixin from "ember-data-table/mixins/default-query-params";
 import { inject as service } from '@ember/service';
+import moment from "moment";
 
 export default Controller.extend(DefaultQueryParamsMixin, {
+  currentSession: service(),
+
   creatingNewSession: false,
   sort: "-started-at",
   size: 10,
 
-  currentSession: service(),
+  dateRegex: /^(?:(\d{1,2})-)?(?:(\d{1,2})-)?(\d{4})$/,
 
   actions: {
     updateModel() {
       this.get("model").update();
-    }
+    },
+
+    setDateFilter(dateString) {
+      debugger;
+      const date = dateString.split('/').join('-'); // Also support slash-separated dates.
+      const match = this.dateRegex.exec(date);
+      if (!match) {
+        this.set('from', null);
+        this.set('to', null);
+        return;
+      }
+      const min = moment(parseInt(match[3]), 'YYYY', true);
+      let unitToAdd;
+      if (match[1] && match[2]) {
+        unitToAdd = 'day';
+        min.set('date', parseInt(match[1]));
+        min.set('month', parseInt(match[2]) - 1); // Count starts from 0
+      } else if (match[1]) {
+        unitToAdd = 'month';
+        min.set('month', parseInt(match[1]) - 1);
+      } else {
+        unitToAdd = 'year';
+      }
+      const max = min.clone().add(1, unitToAdd + 's');
+
+      this.set('from', min.format('YYYY-MM-DD'));
+      this.set('to', max.format('YYYY-MM-DD'));
+      this.set('page', 0);
+    },
+
+    clearDateFilter() {
+      this.set('to', null);
+      this.set('from', null);
+    },
   }
 }
 );
--- app/routes/meetings.js
+++ app/routes/meetings.js
@@ -2,9 +2,18 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 import _ from 'lodash';
+import moment from 'moment';
 
 export default Route.extend(AuthenticatedRouteMixin, DataTableRouteMixin, {
   modelName: "meeting",
+  queryParams: {
+    from: {
+      refreshModel: true,
+    },
+    to: {
+      refreshModel: true,
+    },
+  },
 
   // TODO: Move to new release of ember data table when release
   model(params) { // Overrides Jquery dependency (Ember.$) in package https://github.com/mu-semtech/ember-data-table/commit/247cfbeb94a7224b4a12e9cb6918c7180c894602
@@ -19,6 +28,17 @@ export default Route.extend(AuthenticatedRouteMixin, DataTableRouteMixin, {
     if (params.filter) { options['filter'] = params.filter; }
      _.merge(options, this.mergeQueryOptions(params));
     return this.get('store').query(this.get('modelName'), options);
+  },
+
+  mergeQueryOptions(params) {
+    let options = {};
+    if (params.from) {
+      options['filter[:gte:started-at]'] = moment(params.from, 'YYYY-MM-DD').utc().format();
+    }
+    if (params.to) {
+      options['filter[:lte:started-at]'] = moment(params.to, 'YYYY-MM-DD').utc().format();
+    }
+    return options;
   },
 
   actions: {
