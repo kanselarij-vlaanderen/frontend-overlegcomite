f99a4a1639989bb4a51264bcae59cb804526196a
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-15 16:52:43 +0200

    Linting + conversion to native class

--- app/controllers/agendaitems/agendaitem/delete.js
+++ app/controllers/agendaitems/agendaitem/delete.js
@@ -1,37 +1,38 @@
 import Controller from '@ember/controller';
 import { inject as controller } from '@ember/controller';
+import { action } from "@ember/object";
 
+export default class DeleteAgendaitemsAgendaitemController extends Controller {
+  @controller('agendaitems') parentController;
 
-export default Controller.extend({
-  parentController: controller('agendaitems'),
+  isLoading = false;
 
-  isLoading: false,
-
-  actions: {
-    save() {
-      this.set('isLoading', true);
-      const indexOfCurrent = this.get('model.meeting.sortedAgendaItems').indexOf(this.get('model'));
-      const neighbouringItem = this.get('model.meeting.sortedAgendaItems').objectAt(indexOfCurrent - 1) ||
-        this.get('model.meeting.sortedAgendaItems').objectAt(indexOfCurrent + 1);
-      this.get('model').save()
-        .then(() => {
-          if (neighbouringItem) {
-            this.transitionToRoute('agendaitems.agendaitem', neighbouringItem);
-          } else {
-            this.transitionToRoute('agendaitems');
-          }
-        })
-        .catch(() => {
-          // TODO: Handle error
-        })
-        .finally(() => {
-          this.set('isLoading', false);
-        });
-    },
-
-    cancel() {
-      this.get('model').rollbackAttributes();
-      this.transitionToRoute('agendaitems.agendaitem.index', this.get('model'));
-    },
+  @action
+  save() {
+    this.set('isLoading', true);
+    const sortedItems = this.model.meeting.get('sortedAgendaItems');
+    const indexOfCurrent = sortedItems.indexOf(this.model);
+    const neighbouringItem = sortedItems.objectAt(indexOfCurrent - 1) ||
+      sortedItems.objectAt(indexOfCurrent + 1);
+    this.model.save()
+      .then(() => {
+        if (neighbouringItem) {
+          this.transitionToRoute('agendaitems.agendaitem', neighbouringItem);
+        } else {
+          this.transitionToRoute('agendaitems');
+        }
+      })
+      .catch(() => {
+        // TODO: Handle error
+      })
+      .finally(() => {
+        this.set('isLoading', false);
+      });
   }
-});
+
+  @action
+  cancel() {
+    this.model.rollbackAttributes();
+    this.transitionToRoute('agendaitems.agendaitem.index', this.model);
+  }
+}
