9b3e576587493dcb28c68e58f489bec6c5f0c0a3
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-15 17:04:00 +0200

    Linting + conversion to native class

--- app/controllers/agendaitems/agendaitem/index.js
+++ app/controllers/agendaitems/agendaitem/index.js
@@ -2,44 +2,46 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { NOTIFICATION_TYPE_ID } from 'frontend-overlegcomite/config/constants';
 import { alias } from '@ember/object/computed';
+import { action } from "@ember/object";
 
-export default Controller.extend({
-  addingDocuments: false,
-  addingNotification: false,
+export default class IndexAgendaitemsAgendaitemController extends Controller {
+  addingDocuments = false;
+  addingNotification = false;
 
-  meeting: alias('model.meeting'),
+  @alias('model.meeting') meeting;
 
-  loading: false,
+  loading = false;
 
-  currentSession: service(),
-  store: service(),
+  @service currentSession;
+  @service store;
 
-  init() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
     const notificationType = this.store.findRecord('document-type', NOTIFICATION_TYPE_ID);
     this.set('notificationType', notificationType);
-  },
-
-  actions: {
-    async addDocuments(documents) {
-      this.set('loading', true);
-      this.get('model.documents').pushObjects(documents);
-      await this.get('model').save();
-      this.set('addingDocuments', false);
-      this.set('loading', false);
-    },
-
-    async addNotification([document]) {
-      this.set('loading', true);
-      this.set('model.notification', document);
-      await this.get('model').save();
-      this.set('addingNotification', false);
-      this.set('loading', false);
-    },
-
-    async deleteDocument(document) {
-      this.get('model.documents').popObject(document);
-      await this.get('model').save();
-    }
   }
-});
+
+  @action
+  async addDocuments(documents) {
+    this.set('loading', true);
+    this.model.documents.pushObjects(documents);
+    await this.model.save();
+    this.set('addingDocuments', false);
+    this.set('loading', false);
+  }
+
+  @action
+  async addNotification([document]) {
+    this.set('loading', true);
+    this.set('model.notification', document);
+    await this.model.save();
+    this.set('addingNotification', false);
+    this.set('loading', false);
+  }
+
+  @action
+  async deleteDocument(document) {
+    this.model.documents.popObject(document);
+    await this.model.save();
+  }
+}
