7447d8883f91b3ad253c0b9570fc0a4330f80b5e
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-15 15:35:41 +0200

    Linting + conversion to native class

--- app/controllers/meetings/new.js
+++ app/controllers/meetings/new.js
@@ -1,41 +1,40 @@
 import Controller from '@ember/controller';
 import { inject as controller } from '@ember/controller';
-import { computed } from '@ember/object';
+import { action } from "@ember/object";
 
+export default class NewMeetings extends Controller {
+  @controller('meetings') parentController;
+  isLoading = false;
 
-
-export default Controller.extend({
-  parentController: controller('meetings'),
-  isLoading: false,
-
-  dateForPicker: computed('model.startedAt', function () { // TODO imported datepicker only supports list of dates fro arg 'date'
+  get dateForPicker () { // TODO imported datepicker only supports list of dates fro arg 'date'
     return [this.model.startedAt];
-  }),
+  }
 
-  actions: {
-    setMeetingDate(startedAt) {
-      // startedAt = moment(startedAt).toDate();
-      this.set('model.startedAt', startedAt);
-      // const modified = moment().toDate();
-      // this.set('model.modified', modified);
-    },
+  @action
+  setMeetingDate(startedAt) {
+    // startedAt = moment(startedAt).toDate();
+    this.set('model.startedAt', startedAt);
+    // const modified = moment().toDate();
+    // this.set('model.modified', modified);
+  }
 
-    save() {
-      this.set('isLoading', true);
-      this.model
-        .save()
-        .catch(() => {
-          // TODO: Handle error
-        })
-        .finally(() => {
-          this.set('isLoading', false);
-          this.parentController.send('updateModel');
-          this.transitionToRoute('meetings.index');
-        });
-    },
+  @action
+  save() {
+    this.set('isLoading', true);
+    this.model
+      .save()
+      .catch(() => {
+        // TODO: Handle error
+      })
+      .finally(() => {
+        this.set('isLoading', false);
+        this.parentController.send('updateModel');
+        this.transitionToRoute('meetings.index');
+      });
+  }
 
-    cancel() {
-      this.transitionToRoute('meetings.index');
-    },
-  },
-});
+  @action
+  cancel() {
+    this.transitionToRoute('meetings.index');
+  }
+}
