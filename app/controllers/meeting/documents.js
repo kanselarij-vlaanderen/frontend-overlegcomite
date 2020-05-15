d98d10c1c249400593c177dd22e7514854e0da84
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-15 15:18:37 +0200

    Linting + conversion to native class

--- app/controllers/meeting/documents.js
+++ app/controllers/meeting/documents.js
@@ -2,34 +2,35 @@ import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { DEFAULT_MEETING_DOC_TYPE_ID } from 'frontend-overlegcomite/config/config';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  currentSession: service(),
-  meeting: alias('model'),
+export default class DocumentsMeeting extends Controller {
+  @service currentSession;
+  @alias('model') meeting;
 
-  addingDocuments: false,
-  loading: false,
+  addingDocuments = false;
+  loading = false;
 
-  store: service(),
+  @service store;
 
-  init() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
     const defaultDocumentType = this.store.findRecord('document-type', DEFAULT_MEETING_DOC_TYPE_ID);
     this.set('defaultDocumentType', defaultDocumentType);
-  },
-
-  actions: {
-    async addDocuments(documents) {
-      this.set('loading', true);
-      this.get('model.documents').pushObjects(documents);
-      await this.get('model').save();
-      this.set('addingDocuments', false);
-      this.set('loading', false);
-    },
-
-    async deleteDocument(document) {
-      this.get('model.documents').popObject(document);
-      await this.get('model').save();
-    }
   }
-});
+
+  @action
+  async addDocuments(documents) {
+    this.set('loading', true);
+    this.model.documents.pushObjects(documents);
+    await this.model.save();
+    this.set('addingDocuments', false);
+    this.set('loading', false);
+  }
+
+  @action
+  async deleteDocument(document) {
+    this.model.documents.popObject(document);
+    await this.model.save();
+  }
+}
