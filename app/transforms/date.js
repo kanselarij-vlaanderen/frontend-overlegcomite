ff6e8deef8c8d72a92b9fe9837d41b63d1fc0dfd
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-18 12:16:10 +0200

    Fixed transform imports

--- app/transforms/date.js
+++ app/transforms/date.js
@@ -1,7 +1,8 @@
-import DateTransform from '@ember-data/serializer/transform';
+// Disabled rule, because @ember/data import for transforms doesn't seem to work atm
+import DS from 'ember-data'; // eslint-disable-line ember/use-ember-data-rfc-395-imports
 import moment from 'moment';
 
-export default class extends DateTransform {
+export default class extends DS.DateTransform {
   serialize(date) {
     if (date instanceof Date && !isNaN(date)) {
       return moment(date).format('YYYY-MM-DDZ');
--- app/transforms/datetime.js
+++ app/transforms/datetime.js
@@ -1,4 +1,5 @@
-import DateTransform from '@ember-data/serializer/transform';
+// Disabled rule, because @ember/data import for transforms doesn't seem to work atm
+import DS from 'ember-data'; // eslint-disable-line ember/use-ember-data-rfc-395-imports
 
-export default class extends DateTransform {
+export default class extends DS.DateTransform {
 }
