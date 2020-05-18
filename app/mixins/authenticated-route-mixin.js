7e33eda0419e3c4dfe7fc6b27e5c35d59d63e6f2
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-18 10:13:10 +0200

    Linting

--- app/mixins/authenticated-route-mixin.js
+++ app/mixins/authenticated-route-mixin.js
@@ -1,5 +1,5 @@
 import Mixin from '@ember/object/mixin';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin'; // eslint-disable-line ember/no-mixins
 import ENV from 'frontend-overlegcomite/config/environment';
 
 export default Mixin.create(AuthenticatedRouteMixin, { // eslint-disable-line ember/no-new-mixins
