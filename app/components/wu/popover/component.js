ef6ee2c592ccfd912656c9c17d4b15d1c350aeb7
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-19 17:19:30 +0200

    Re-implement popover with ember-attacher

--- app/components/wu/popover/component.js
+++ app/components/wu/popover/component.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 
 export default class extends Component {
   @tracked isOpen = false;
@@ -14,6 +15,11 @@ export default class extends Component {
       }
     }
   }
+
+  get uniqueId() {
+    return guidFor(this);
+  }
+
   get class() {
     const classes = ['vl-popover'];
     const alignmentOptions = ['left', 'center', 'right'];
@@ -29,6 +35,20 @@ export default class extends Component {
     return classes.join(' ');
   }
 
+  get placement () {
+    const al = this.args.alignment;
+    const alignmentOptions = ['left', 'center', 'right'];
+    if (al && alignmentOptions.includes(al)) {
+      if (al === 'left') {
+        return 'bottom-start';
+      } else if (al === 'center') {
+        return 'bottom';
+      } else if (al === 'right') {
+        return 'bottom-end';
+      }
+    }
+  }
+
   get buttonText () {
     return this.args.buttonText || 'Meer opties';
   }
@@ -36,5 +56,10 @@ export default class extends Component {
   @action
   close () {
     this.isOpen = false;
+  }
+
+  @action
+  setOpenState (open) {
+    this.isOpen = open;
   }
 }
