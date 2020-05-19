af770979033e7cf8d6dc79624c479442677a274b
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-19 16:15:16 +0200

    Improved popover, without ember-popper

--- app/components/wu/popover/component.js
+++ app/components/wu/popover/component.js
@@ -1,8 +1,40 @@
 import Component from '@glimmer/component';
-import { guidFor } from '@ember/object/internals';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class extends Component {
-  get uniqueId() {
-    return guidFor(this);
+  @tracked isOpen = false;
+  requiredArgs = ['alignment'];
+
+  constructor () {
+    super(...arguments);
+    for (const arg of this.requiredArgs) {
+      if (this.args[arg] === undefined) {
+        throw new Error(`"${arg}" is a required argument`);
+      }
+    }
+  }
+  get class() {
+    const classes = ['vl-popover'];
+    const alignmentOptions = ['left', 'center', 'right'];
+    if (this.args.alignment && alignmentOptions.includes(this.args.alignment)) {
+      classes.push(`vl-popover--align-${this.args.alignment}`);
+    }
+    if (this.args.closeable) {
+      classes.push('vl-popover--closable');
+    }
+    if (this.isOpen) {
+      classes.push('js-vl-popover--open');
+    }
+    return classes.join(' ');
+  }
+
+  get buttonText () {
+    return this.args.buttonText || 'Meer opties';
+  }
+
+  @action
+  close () {
+    this.isOpen = false;
   }
 }
