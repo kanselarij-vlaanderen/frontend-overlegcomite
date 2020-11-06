b0d5ab3d748abc4c5be33da5985f777199802757
Michaël Dierick <michael.dierick@redpencil.io>
2020-11-06 15:36:23 +0100

    feat: also create an identifier object when creating a new user

--- app/controllers/user-management/new-user.js
+++ app/controllers/user-management/new-user.js
@@ -4,10 +4,27 @@ import { task } from 'ember-concurrency';
 
 export default class UserManagementNewUserController extends Controller {
   @(task(function * (model) {
-    yield (yield model.get('account')).save();
+    yield this.addIdentifier(); // see "addIdentifier" doc
+
+    const account = yield this.model.get('account');
+    const identifier = yield this.model.get('identifier');
+    yield identifier.save();
+    yield account.save();
     yield model.save();
     this.transitionToRoute('user-management');
   })) saveModel;
+
+
+  async addIdentifier() {
+    /* VO-id is stored as a direct property on account, as well as a property of
+     * an "identifier"-object, which is related to a user. Below we generate the "identifier"-object,
+     * based on the VO-id which has been set on the account  */
+    const account = await this.model.get('account');
+    const identifier = this.store.createRecord('identifier', {
+      notation: account.voId
+    });
+    return this.model.set('identifier', identifier);
+  }
 
   @action
   async cancel () {
