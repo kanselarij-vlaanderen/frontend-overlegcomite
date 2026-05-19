ea09480947807e99f006e325c9cd65f241e0153b
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-05-19 09:23:44 +0200

    Improve meetings list
    Fix pagination, add links, improve loading state

--- app/controllers/meetings.js
+++ app/controllers/meetings.js
@@ -1,9 +1,23 @@
 import Controller from '@ember/controller';
-import { query} from '@warp-drive/utilities/json-api'
+import { query } from '@warp-drive/utilities/json-api'
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class MeetingsController extends Controller {
+  @tracked sort = "-started-at";
+  @tracked page = 0;
+  @tracked size = 10;
 
   get meetingsQuery() {
-    return query('meeting')
+    return query('meeting', {
+      sort: this.sort,
+      "page[size]": this.size,
+      "page[number]": this.page
+    })
+  }
+
+  @action
+  goToMeeting(meeting) {
+    console.warn('TODO: go to meeting route')
   }
 }
--- app/routes/meetings.js
+++ app/routes/meetings.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import { query } from '@warp-drive/utilities/json-api';
 
 export default class MeetingsRoute extends Route {
