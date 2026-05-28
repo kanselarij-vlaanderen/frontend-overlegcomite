6f31a6ce28a8176af7b2e93ecb9e97be8ffb5b1b
Erika Pauwels <erika.pauwels@gmail.com>
2026-05-28 11:16:52 +0200

    Refactor argument that is passed to meeting-form component

--- app/components/meeting-form.js
+++ app/components/meeting-form.js
@@ -1,28 +1,21 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class MeetingForm extends Component {
-
-  get meeting() {
-    return this.args.meeting;
-  }
-
   get startedAtDate() {
-    const date = this.meeting.startedAt.toPlainDate();
+    const date = this.args.meeting.startedAt.toPlainDate();
     return date.toString();
   }
 
   @action
   setStartedAtDate(dateString) {
-    const dateTime = this.meeting.startedAt;
-    const newDate = Temporal.PlainDate.from(dateString)
+    const dateTime = this.args.meeting.startedAt;
+    const newDate = Temporal.PlainDate.from(dateString);
 
-    this.meeting.startedAt = dateTime.with({
-        year: newDate.year,
-        month: newDate.month,
-        day: newDate.day
+    this.args.meeting.startedAt = dateTime.with({
+      year: newDate.year,
+      month: newDate.month,
+      day: newDate.day
     });
   }
-
 }
