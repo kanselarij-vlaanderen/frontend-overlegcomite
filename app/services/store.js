880dc0d76c81d8185c39f260543146ff31527a9b
Erika Pauwels <erika.pauwels@gmail.com>
2026-05-28 11:07:48 +0200

    Add handler to store to set Content-Type header on JSON:API requests

--- app/services/store.js
+++ app/services/store.js
@@ -1,13 +1,26 @@
-import { useLegacyStore } from '@warp-drive/legacy'
+import { useLegacyStore } from '@warp-drive/legacy';
 import { JSONAPICache } from '@warp-drive/json-api';
 import Meetings from '../schemas/meetings';
 import DatetimeTransform  from '../transforms/datetime';
 
-export default useLegacyStore({
+const JsonApiHeaderHandler = {
+  request(context, next) {
+    const { request } = context;
+    const updatedHeaders = request.headers.clone();
+    updatedHeaders.set('Content-Type', 'application/vnd.api+json');
+    request.headers = updatedHeaders;
+    return next(request);
+  }
+};
+
+const legacyStore = useLegacyStore({
   linksMode: false,
   legacyRequests: true,
   modelFragments: true,
   cache: JSONAPICache,
+  handlers: [
+    JsonApiHeaderHandler,
+  ],
   schemas: [
     Meetings
   ],
@@ -15,3 +28,5 @@ export default useLegacyStore({
     DatetimeTransform.create(),
   ]
 });
+
+export default legacyStore;
