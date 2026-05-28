f36365ff754b9e209d8d006c39fe28adb23e1ff0
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-05-28 15:37:43 +0200

    Add datetime short format

--- app/helpers/datetime-format.js
+++ app/helpers/datetime-format.js
@@ -1,7 +1,25 @@
-export default function datetimeFormat(dateTime) {
+function makeFormat(style) {
+  return Intl.DateTimeFormat(undefined, { dateStyle: style, timeStyle: style})
+}
+
+const FORMATS = {
+  short: Intl.DateTimeFormat(undefined, {
+    day: "numeric", month: "short", year: "2-digit",
+    hour: "2-digit", minute: "2-digit"
+  }),
+  medium: makeFormat('medium'),
+  long: makeFormat('long'),
+  full: makeFormat('full'),
+}
+
+export default function datetimeFormat(dateTime, style_) {
+  const style = style_ || 'short';
+
   if (!dateTime) {
     return dateTime;
   }
 
-  return dateTime.toLocaleString()
+  const format = FORMATS[style];
+
+  return format.format(dateTime.toInstant());
 }
