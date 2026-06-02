09f3d6db570099e4d557b9dbfc7dcc2f8efb9310
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-06-02 17:13:45 +0200

    Implement document version editing functionality.

--- app/components/document-version-buttons.js
+++ app/components/document-version-buttons.js
@@ -1,3 +1,33 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { cacheKeyFor } from '@warp-drive/core';
 
-export default class DocumentVersionButtons extends Component {}
+export default class DocumentVersionButtons extends Component {
+  @service store;
+
+  @tracked isEditingAccessLevel = false;
+
+  get accessLevelOptions() {
+    return this.store.query('access-level', { sort: 'priority' });
+  }
+
+  @action
+  async saveAccessLevel() {
+    this.isEditingAccessLevel = false;
+    await this.args.version.save();
+  }
+  @action
+  cancelEditingAccessLevel() {
+    this.store.cache.rollbackRelationships(cacheKeyFor(this.args.version));
+    this.isEditingAccessLevel = false;
+  }
+
+  @action
+  toggleConfidential() {
+    const version = this.args.version;
+    version.confidential = !version.confidential;
+    version.save();
+  }
+}
