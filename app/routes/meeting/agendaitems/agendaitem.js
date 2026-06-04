0dd57d9f6cfe012b4ee01c0290a079ffe0a07ec2
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-06-04 10:38:48 +0200

    Fix: agendaitem route case loading
    Move case loading to the model loader.

--- app/routes/meeting/agendaitems/agendaitem.js
+++ app/routes/meeting/agendaitems/agendaitem.js
@@ -4,17 +4,17 @@ import { service } from '@ember/service';
 export default class MeetingAgendaitemsAgendaitemRoute extends Route {
   @service store;
 
-  model({ agendaitem_id }) {
+  async model({ agendaitem_id }) {
     // eslint-disable-next-line warp-drive/no-legacy-request-patterns
-    return this.store.findRecord('agendaitem', agendaitem_id, {
+    const agendaitem = this.store.findRecord('agendaitem', agendaitem_id, {
       include: ['submitters', 'case']
     });
+    this.case = await agendaitem.case;
+    return agendaitem;
   }
 
   setupController(controller, model) {
     super.setupController(controller, model);
-    model.case.then((case_) => {
-      controller.set('caseIdentifier', case_.identifier);
-    })
+    controller.caseIdentifier = this.case.identifier;
   }
 }
