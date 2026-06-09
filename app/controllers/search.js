dde197f39c6f8979e7dcab6af948ec085a4119cf
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-06-09 15:01:30 +0200

    Add: search notifications toggle

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class SearchController extends Controller {
   @tracked searchText = '';
+  @tracked notificationsOnly = false;
 
   @tracked sort = '';
   @tracked page = 0;
