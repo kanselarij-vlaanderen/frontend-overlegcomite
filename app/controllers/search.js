a72dad2251e29a3605cdd0b855b9b72d421ec207
Erika Pauwels <erika.pauwels@gmail.com>
2026-06-12 12:14:08 +0200

    Rearrange styling of search header

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -2,10 +2,10 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
 export default class SearchController extends Controller {
-  @tracked searchText = '';
+  @tracked searchText;
   @tracked notificationsOnly = false;
 
-  @tracked sort = '';
+  @tracked sort = '-meeting-date';
   @tracked page = 0;
-  @tracked size = 10;
+  @tracked size = 20;
 }
