2bd1466e5fd89633e8372fc8ff586987851dd96a
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-06-16 13:16:36 +0200

    Add: plausible events for errors

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,20 +1,28 @@
- import Controller from '@ember/controller';
+import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
 import extractErrors from '../helpers/extract-errors';
 
 export default class ApplicationController extends Controller {
   @service toaster;
+  @service plausible;
 
   @action
-  showError(error) {
+  async showError(error) {
     const errors = extractErrors(error);
-    for (const error of errors) {
-      this.toaster.notify(error.description, error.title, {
-        type: 'error',
-        icon: 'alert-circle',
-        timeOut: 5000
-      })
-    }
+    await Promise.allSettled(
+      errors.map((error) => {
+        this.toaster.notify(error.description, error.title, {
+          type: 'error',
+          icon: 'alert-circle',
+          timeOut: 5000,
+        });
+
+        return this.plausible.trackEvent('Error', {
+          title: error.title,
+          description: error.description,
+        });
+      }),
+    );
   }
 }
--- app/routes/error.js
+++ app/routes/error.js
@@ -1,3 +1,20 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import extractErrors from '../helpers/extract-errors';
 
-export default class ErrorRoute extends Route { }
+export default class ErrorRoute extends Route {
+  @service plausible;
+
+  async setupController(_controller, model) {
+    await Promise.allSettled(
+      extractErrors(model).map((error) => {
+        return this.plausible.trackEvent('Error', {
+          title: error.title,
+          description: error.description,
+        });
+      }),
+    );
+
+    return super.setupController(...arguments);
+  }
+}
