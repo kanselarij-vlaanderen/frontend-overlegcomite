153e0067b11199a7b608800610128876cc5bd879
Erika Pauwels <erika.pauwels@gmail.com>
2026-06-22 11:26:06 +0200

    Remove unused import

--- app/models/document-version.js
+++ app/models/document-version.js
@@ -1,4 +1,4 @@
-import Model, { attr, belongsTo, hasMany } from '@warp-drive/legacy/model';
+import Model, { attr, belongsTo } from '@warp-drive/legacy/model';
 
 export default class DocumentVersionModel extends Model {
   @attr('datetime') created;
